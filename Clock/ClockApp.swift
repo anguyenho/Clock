2bbdf4c02dff217ab129cfda0f198f10dc5c291c
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2024-11-08 08:52:00 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Isla Russell-Howes on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
