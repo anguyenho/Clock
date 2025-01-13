89de8c81215500a61b8d3a2cb3845cbc913cfcba
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-01-13 09:59:55 -0500

    Made an extracted view

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct AlarmsView: View {
     var body: some View {
         NavigationStack {
-            Text("Alarms")
-                .navigationTitle("Alarms")
+            ExtractedView(time: "8:00", meridiem: "AM")
         }
     }
 }
 
 #Preview {
     LandingView()
+}
+
+struct ExtractedView: View {
+   
+    let time: String
+    let meridiem: String
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text(time)
+                    .font(.system(size:64.0, weight: .thin, design: .default))
+                Text(meridiem)
+                    .font(.system(.largeTitle, design: .default, weight: .thin))
+                Spacer()
+                Toggle("", isOn: Binding.constant(true))
+            }
+            .padding()
+        }
+        .navigationTitle("Alarms")
+    }
 }
